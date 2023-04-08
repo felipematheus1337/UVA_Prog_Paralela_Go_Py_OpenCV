fb3d722aa34d0e1e692e78322e161df0aafc3180
felipematheus1337 <lipehbr@gmail.com>
2023-04-08 17:28:54 -0300

    Endpoint para diversas imagens criado!

--- Go_Microsservice/services/fileservice.go
+++ Go_Microsservice/services/fileservice.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"fmt"
+	"mime/multipart"
 	"net/http"
 	"path"
 	"strings"
@@ -82,5 +83,70 @@ func EnviarImagem(w http.ResponseWriter, r *http.Request) {
 	} else {
 		http.Error(w, "Método não permitiodo", http.StatusMethodNotAllowed)
 	}
+}
 
+func EnviarImagens(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method == "POST" {
+		var waitGroup sync.WaitGroup
+
+		err := r.ParseMultipartForm(int64(tamanhoMaximoDoArquivo))
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		formulario := r.MultipartForm
+
+		arquivos, ok := formulario.File["arquivos"]
+		if !ok {
+			http.Error(w, "Nenhuma imagem encontrada", http.StatusBadRequest)
+			return
+		}
+
+		numeroDeArquivos := len(arquivos)
+
+		waitGroup.Add(numeroDeArquivos * 2)
+
+		for _, arquivo := range arquivos {
+
+			tamanho := make(chan bool)
+			extensao := make(chan bool)
+
+			go func(arquivo *multipart.FileHeader) {
+				tamanho <- verificarTamanhoPermitido(arquivo.Size)
+				waitGroup.Done()
+			}(arquivo)
+
+			extensaoArquivo := strings.ToLower(path.Ext(arquivo.Filename))
+
+			go func(extensaoArquivo string) {
+				extensao <- verificarExtensaoPermitida(extensaoArquivo)
+				waitGroup.Done()
+			}(extensaoArquivo)
+
+			verifyTamanho := <-tamanho
+			if !verifyTamanho {
+				w.Write([]byte("Tamanho inválido, imagens até 10MB!"))
+				return
+			}
+
+			verifyExtensao := <-extensao
+			if !verifyExtensao {
+				w.Write([]byte("Extensão inválida! Apenas png, jpg ou jpeg"))
+				return
+			}
+		}
+
+		waitGroup.Wait()
+
+		for _, arquivo := range arquivos {
+			nomeDoArquivo := arquivo.Filename
+			fmt.Fprintf(w, "Arquivo %s enviado com sucesso!\n", nomeDoArquivo)
+		}
+
+	} else {
+		http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
+	}
 }
