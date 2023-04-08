a44e2324d03dad10ebc8b7ee45f2b1157f6231b8
felipematheus1337 <lipehbr@gmail.com>
2023-04-08 12:13:43 -0300

    Adicionado endpoint para receber uma imagem!

--- Go_Microsservice/main.go
+++ Go_Microsservice/main.go
@@ -12,7 +12,8 @@ import (
 func main() {
 
 	router := mux.NewRouter()
-	router.HandleFunc("/images", services.EnviarImagens).Methods(http.MethodGet)
+	router.HandleFunc("/upload/single", services.EnviarImagens).Methods(http.MethodPost)
+	http.Handle("/", router)
 
 	fmt.Println("Escutando na porta 8081")
 	log.Fatal(http.ListenAndServe(":8081", router))
--- Go_Microsservice/services/fileservice.go
+++ Go_Microsservice/services/fileservice.go
@@ -3,10 +3,42 @@ package services
 import (
 	"fmt"
 	"net/http"
+	"path/filepath"
 )
 
+var tamanhoMaximoDoArquivo = 10 * 1024 * 1024
+
 func EnviarImagens(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Chegou")
-	w.Write([]byte("Chegou .."))
-	w.WriteHeader(http.StatusOK)
+
+	if r.Method == "POST" {
+		if r.ContentLength > int64(tamanhoMaximoDoArquivo) {
+			w.Write([]byte("Não é possível fazer upload de arquivo com tamanho maior de 10mb"))
+			return
+		}
+
+		arquivo, cabecalho, err := r.FormFile("arquivo")
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		extensao := filepath.Ext(cabecalho.Filename)
+
+		if extensao != ".jpg" && extensao != ".png" && extensao != ".jpeg" {
+			w.Write([]byte("Apenas arquivos .jpg, .png e .jpeg são permitidos"))
+			return
+		}
+
+		defer arquivo.Close()
+
+		nomeDoArquivo := cabecalho.Filename
+
+		fmt.Println(arquivo)
+
+		fmt.Fprintf(w, "Arquivo %s enviado com sucesso!", nomeDoArquivo)
+	} else {
+		http.Error(w, "Método não permitiodo", http.StatusMethodNotAllowed)
+	}
+
 }
